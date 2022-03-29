f4dbbbc6036084f680bb545afd149f32990359ff
Yurii Kovalchuk <zt1552@gmail.com>
2022-03-29 19:32:08 +0300

    register and login

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\EditProductFormType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +15,67 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", methods="GET", name="homepage")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $productList = $entityManager->getRepository(Product::class)->findAll();
+//        dd($productList);
+
         return $this->render('main/default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'controller_name' => 'DefaultController'
+        ]);
+    }
+
+//    /**
+//     * @Route("/product-add", name="product_add")
+//     */
+//    public function productAdd(): Response
+//    {
+//        $product = new Product();
+//        $product->setTitle('Product_'.rand(1, 500));
+//        $product->setPrice(rand(100, 1000));
+//        $product->setQuantity(rand(1, 10));
+//        $product->setDecription('something_descr_'.rand(200, 300));
+//
+//        $entityManager = $this->getDoctrine()->getManager();
+//        $entityManager->persist($product);
+//        $entityManager->flush();
+//
+//
+//        return $this->redirectToRoute('homepage');
+//    }
+
+    /**
+     * @Route("/edit-product/{id}", methods="GET|POST", name="product_edit", requirements={"id" = "\d+"})
+     * @Route("/add-product", methods="GET|POST", name="product_add")
+     */
+    public function editProduct(Request $request, int $id = null): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        if ($id) {
+            $product = $entityManager->getRepository(Product::class)->find($id);
+        } else {
+            $product = new Product();
+        }
+
+        $form = $this->createForm(EditProductFormType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product_edit', ['id' => $product->getId()]);
+        }
+
+//        dd($product, $form);
+
+        return $this->render('main/default/edit_product.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
