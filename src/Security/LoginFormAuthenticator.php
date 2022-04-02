39d687f73e1017ff1d226ef9ddc94ce32018c816
Yurii Kovalchuk <zt1552@gmail.com>
2022-04-02 17:13:07 +0300

    with access control

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -50,7 +50,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('homepage'));
+        return new RedirectResponse($this->urlGenerator->generate('main_profile_index'));
 //        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
